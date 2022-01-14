b6d013a906faa703b534daee49b2e8c24048db2b
Artem Zhukov <at.zhukov@gmail.com>
2022-01-14 22:33:42 +0100

    Add argument parser

--- Sources/printds/main.swift
+++ Sources/printds/main.swift
@@ -1,1 +1,16 @@
-print("Hello, world!")
+import ArgumentParser
+
+struct App: ParsableCommand {
+    
+    static var configuration = CommandConfiguration(
+        commandName: "printds",
+        abstract: "Print double sided documents manually with ease.",
+        version: "0.0.1"
+    )
+
+    @Argument(help: "The path to the document.")
+    var input: String
+    
+}
+
+App.main()
