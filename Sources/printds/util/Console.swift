c05e977530094ba11a02c1d4e82c0c48aacc7f25
Artem Zhukov <at.zhukov@gmail.com>
2022-01-16 17:22:26 +0100

    Add documentation

--- Sources/printds/util/Console.swift
+++ Sources/printds/util/Console.swift
@@ -17,25 +17,35 @@ class Console: Decodable {
         case reset = "\u{001B}[0m"
     }
     
+    /// Print a message to the console.
+    /// - parameter message: The message to be printed.
     func log(_ message: String) {
         print(message)
     }
     
+    /// Print a success message (in green) to the console.
+    /// - parameter message: The message to be printed.
     func success(_ message: String) {
         output(message, style: .green)
     }
     
+    /// Print an info message (in cyan) to the console.
+    /// - parameter message: The message to be printed.
     func info(_ message: String) {
         output(message, style: .cyan)
     }
     
-    func prompt(_ message: String) {
-        output(message, style: .magenta)
-        _ = readLine()
-    }
-    
+    /// Print an error message (in red) to the console.
+    /// - parameter message: The message to be printed.
     func error(_ message: String) {
         output(message, style: .red)
+    }
+    
+    /// Print a promt (in magenta) to the console, and wait for
+    /// - parameter message: The message to be printed.
+    @discardableResult func prompt(_ message: String) -> String? {
+        output(message, style: .magenta)
+        return readLine()
     }
     
     private func output(_ message: String, style: Style) {
