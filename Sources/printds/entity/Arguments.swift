9b2b6a034c94dcd34f12970b6c203d65866cd9d6
Artem Zhukov <at.zhukov@gmail.com>
2022-01-18 02:02:22 +0100

    Separate init & validation

--- Sources/printds/entity/Arguments.swift
+++ Sources/printds/entity/Arguments.swift
@@ -7,6 +7,7 @@
 
 import Foundation
 
+/// A struct responsible for storing and validating arguments for this program.
 struct Arguments {
     
     let input: String
@@ -15,15 +16,23 @@ struct Arguments {
     let from: Int?
     let to: Int?
     
+    /// Initializes this struct.
+    /// - throws: If the validation fails.
     init(input: String, output: String?, from: Int?, to: Int?) throws {
         self.input = input
         self.output = output
+        self.from = from
+        self.to = to
+        try self.validate()
+    }
+    
+    /// Checks whether the arguments are valid.
+    /// - throws: If the validation fails.
+    private func validate() throws {
         if let from = from, let to = to, from > to {
             throw Exception
                 .because("The value of option 'from' (\(from)) is larger than the value of 'to' (\(to)).")
         }
-        self.from = from
-        self.to = to
     }
     
 }
