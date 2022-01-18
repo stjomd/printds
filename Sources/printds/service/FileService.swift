ef688eece21eae0f4546f46ae2131c91a9c948d3
Artem Zhukov <at.zhukov@gmail.com>
2022-01-18 17:12:22 +0100

    Use URL(fileURLWithPath:) instead of prepending "file://"

--- Sources/printds/service/FileService.swift
+++ Sources/printds/service/FileService.swift
@@ -16,9 +16,8 @@ class FileService: Decodable {
     /// - throws: An exception is thrown if the file couldn't be found.
     public func locate(_ path: String) throws -> URL {
         let correctedPath = path.replacingOccurrences(of: "\\ ", with: " ")
-        let globalUrl = try self.url(from: correctedPath)
-        let localUrl = try self.url(from: FileManager.default.currentDirectoryPath)
-                               .appendingPathComponent(correctedPath)
+        let globalUrl = url(from: correctedPath)
+        let localUrl = url(from: FileManager.default.currentDirectoryPath).appendingPathComponent(correctedPath)
         if FileManager.default.fileExists(atPath: localUrl.path) {
             return localUrl
         } else if FileManager.default.fileExists(atPath: globalUrl.path) {
@@ -71,12 +70,8 @@ class FileService: Decodable {
     /// Creates a URL instance from a path string.
     /// - parameter path: The path to a file, a string.
     /// - returns: The URL with the equivalent path.
-    /// - throws: An exception is thrown if the URL cannot be constructed.
-    private func url(from path: String) throws -> URL {
-        guard let url = URL(string: "file://" + path) else {
-            throw Exception.because("Invalid path \(path).")
-        }
-        return url
+    private func url(from path: String) -> URL {
+        return URL(fileURLWithPath: path)
     }
     
     /// Checks whether the path specidied by a URL is a directory.
