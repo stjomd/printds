1ca09a38aa58ac28f0acee2644696f3c2dbd7335
Artem Zhukov <at.zhukov@gmail.com>
2022-01-18 22:37:22 +0100

    Add communicator tests

---
+++ Tests/printdsTests/mock/MockPrintService.swift
@@ -0,0 +1,20 @@
+//
+//  MockPrintService.swift
+//  printdsTests
+//
+//  Created by Artem Zhukov on 18.01.22.
+//
+
+import Foundation
+import PDFKit
+@testable import printds
+
+class MockPrintService: PrintService {
+    
+    private(set) var counter = 0
+    
+    override func print(_ document: PDFDocument) throws {
+        counter += 1
+    }
+    
+}
