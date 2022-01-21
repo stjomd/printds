3e29547d3a0d2f28f621b39c9bc945073d72d7ce
Artem Zhukov <at.zhukov@gmail.com>
2022-01-22 00:54:38 +0100

    Update version

--- Sources/printds/App.swift
+++ Sources/printds/App.swift
@@ -38,7 +38,7 @@ struct App: ParsableCommand {
         It's also possible to save processed PDF files to print them later. \
         Moreover, you can specify which pages of the document should be processed.
         """,
-        version: "1.1.3"
+        version: "1.1.4"
     )
 
     @Argument(help: "The path to the document.")
