4552424d132f3669459a5f07a9b540bccc0adbe9
Artem Zhukov <at.zhukov@gmail.com>
2022-01-22 00:33:20 +0100

    Refactor

--- Sources/printds/util/Communicator.swift
+++ Sources/printds/util/Communicator.swift
@@ -33,31 +33,13 @@ class Communicator: Decodable {
         let noun = (split.pageCount == 1) ? "sheet" : "sheets"
         console.info("You will need \(split.pageCount) \(noun) of paper.")
         // Output
-        if let odd = split.odd {
-            do {
-                try self.output(odd, suffix: "odd", args: args)
-            } catch Exception.initiated(let message) {
-                console.error(message)
-            }
-        }
-        if let even = split.even {
-            if args.output == nil {
-                console.info("Turn the printed pages over, and insert them into the paper tray.")
-                console.prompt("Press enter to continue.")
-            }
-            do {
-                try self.output(even, suffix: "even", args: args)
-            } catch Exception.initiated(let message) {
-                console.error(message)
-            }
-            if args.output != nil {
-                console.info("First print the odd pages, then the even ones.")
-            }
-        }
+        let success = try self.outputSplit(docs: split, args: args)
         // Finish
-        let saved = document.pageCount - split.pageCount
-        let word = (saved == 1) ? "sheet" : "sheets"
-        console.success("Done. You've saved \(saved) \(word) of paper.")
+        if success {
+            let saved = document.pageCount - split.pageCount
+            let word = (saved == 1) ? "sheet" : "sheets"
+            console.success("Done. You've saved \(saved) \(word) of paper.")
+        }
     }
     
     /// Runs the program in the singlesided mode.
@@ -78,6 +60,44 @@ class Communicator: Decodable {
     }
     
     // MARK: - Helpers
+    
+    /// Proceeds to output a split document.
+    /// - parameters:
+    ///   - docs: The split PDF document.
+    ///   - args: The program's arguments.
+    /// - returns: A boolean value indicating whether at least one of the files has been saved. Always true if printing.
+    /// - throws: An exception is thrown if something went wrong while printing/saving.
+    private func outputSplit(docs: SplitPDFDocument, args: Arguments) throws -> Bool {
+        var (savedOdd, savedEven) = (false, false)
+        if let odd = docs.odd {
+            do {
+                try self.output(odd, suffix: "odd", args: args)
+                savedOdd = true
+            } catch Exception.initiated(let message) {
+                console.error(message)
+            }
+        }
+        if let even = docs.even {
+            if args.output == nil {
+                console.info("Turn the printed pages over, and insert them into the paper tray.")
+                console.prompt("Press enter to continue.")
+            }
+            do {
+                try self.output(even, suffix: "even", args: args)
+                savedEven = true
+            } catch Exception.initiated(let message) {
+                console.error(message)
+            }
+            if args.output != nil && (savedOdd || savedEven) {
+                console.info("First print the odd pages, then the even ones.")
+            }
+        }
+        if let _ = args.output { // saving
+            return savedOdd || savedEven
+        } else {
+            return true
+        }
+    }
     
     /// Either prints or saves the document, depending on the program's arguments.
     /// - parameters:
