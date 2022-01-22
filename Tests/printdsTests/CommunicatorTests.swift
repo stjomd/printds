2c0a748d078b7f4188a6c40512b4a16c329f296a
Artem Zhukov <at.zhukov@gmail.com>
2022-01-22 22:14:41 +0100

    Add communicator overwrite tests

--- Tests/printdsTests/CommunicatorTests.swift
+++ Tests/printdsTests/CommunicatorTests.swift
@@ -16,19 +16,22 @@ class CommunicatorTests: XCTestCase {
     private var directory: String!
     private var identifier: String!
     
+    private var console: TestableConsole!
+    private var fileService: TestableFileService!
     private var printService: MockPrintService!
     private var communicator: Communicator!
 
     override func setUpWithError() throws {
         self.directory = try Shell.exec("pwd")
         self.identifier = UUID().description
-        let fileService = FileService(console: TestableConsole())
+        self.console = TestableConsole()
+        self.fileService = TestableFileService(console: self.console)
         self.printService = MockPrintService() // doesn't open the print panel, but increments a counter
         self.communicator = Communicator(
-            console: TestableConsole(),
-            fileService: fileService,
-            printService: printService,
-            documentService: DocumentService(fileService: fileService)
+            console: self.console,
+            fileService: self.fileService,
+            printService: self.printService,
+            documentService: DocumentService(fileService: self.fileService)
         )
         // Create a document
         MockDocumentService.mockDocument(size: 5)
@@ -47,23 +50,29 @@ class CommunicatorTests: XCTestCase {
 
     func test_doublesided_shouldPrintTwice_whenOutputIsNil() throws {
         XCTAssertEqual(printService.counter, 0)
+        XCTAssertEqual(fileService.counter, 0)
         let args = try Arguments(input: "\(directory!)/\(identifier!).pdf", output: nil, from: nil, to: nil)
         try communicator.doublesided(args)
         XCTAssertEqual(printService.counter, 2)
+        XCTAssertEqual(fileService.counter, 0)
     }
     
     func test_doublesided_shouldPrintOnce_whenOnlyOnePage() throws {
         XCTAssertEqual(printService.counter, 0)
+        XCTAssertEqual(fileService.counter, 0)
         let args = try Arguments(input: "\(identifier!).pdf", output: nil, from: 2, to: 2)
         try communicator.doublesided(args)
         XCTAssertEqual(printService.counter, 1)
+        XCTAssertEqual(fileService.counter, 0)
     }
     
     func test_doublesided_shouldSaveTwoDocs_whenOutputNotNil() throws {
         XCTAssertEqual(printService.counter, 0)
+        XCTAssertEqual(fileService.counter, 0)
         let args = try Arguments(input: "\(directory!)/\(identifier!).pdf", output: ".", from: nil, to: nil)
         try communicator.doublesided(args)
         XCTAssertEqual(printService.counter, 0)
+        XCTAssertEqual(fileService.counter, 2)
         let ls = try Shell.exec("ls \(identifier!)*.pdf").split(separator: "\n")
         XCTAssertTrue(ls.contains("\(identifier!).odd.pdf"))
         XCTAssertTrue(ls.contains("\(identifier!).even.pdf"))
@@ -71,32 +80,63 @@ class CommunicatorTests: XCTestCase {
     
     func test_doublesided_shouldSaveOneDoc_whenOutputNotNilAndOnlyOnePage() throws {
         XCTAssertEqual(printService.counter, 0)
+        XCTAssertEqual(fileService.counter, 0)
         let args = try Arguments(input: "\(identifier!).pdf", output: ".", from: 3, to: 3)
         try communicator.doublesided(args)
         XCTAssertEqual(printService.counter, 0)
+        XCTAssertEqual(fileService.counter, 1)
         let ls = try Shell.exec("ls \(identifier!)*.pdf").split(separator: "\n")
         XCTAssertTrue(ls.contains("\(identifier!).odd.pdf"))
         XCTAssertFalse(ls.contains("\(identifier!).even.pdf"))
     }
     
+    func test_doublesided_shouldNotThrow_whenDeclinedToOverwrite() throws {
+        XCTAssertEqual(fileService.counter, 0)
+        try fileService.save(PDFDocument(), named: "\(identifier!).odd.pdf", to: ".")
+        try fileService.save(PDFDocument(), named: "\(identifier!).even.pdf", to: ".")
+        XCTAssertEqual(fileService.counter, 2)
+        // Test if throws
+        XCTAssertEqual(printService.counter, 0)
+        XCTAssertEqual(fileService.counter, 2)
+        let args = try Arguments(input: "\(identifier!).pdf", output: ".", from: nil, to: nil)
+        console.response = "n"
+        XCTAssertNoThrow(try communicator.doublesided(args))
+        XCTAssertEqual(printService.counter, 0)
+        XCTAssertEqual(fileService.counter, 4)
+    }
+    
     func test_singlessided_shouldPrintOnce_whenOutputIsNil() throws {
         XCTAssertEqual(printService.counter, 0)
+        XCTAssertEqual(fileService.counter, 0)
         let args = try Arguments(input: "\(directory!)/\(identifier!).pdf", output: nil, from: nil, to: nil)
         try communicator.singlesided(args)
         XCTAssertEqual(printService.counter, 1)
+        XCTAssertEqual(fileService.counter, 0)
     }
     
     func test_singlessided_shouldSaveOneDoc_whenOutputNotNil() throws {
         XCTAssertEqual(printService.counter, 0)
+        XCTAssertEqual(fileService.counter, 0)
         let args = try Arguments(input: "\(identifier!).pdf", output: ".", from: nil, to: nil)
         try communicator.singlesided(args)
         XCTAssertEqual(printService.counter, 0)
+        XCTAssertEqual(fileService.counter, 1)
         let ls = try Shell.exec("ls \(identifier!)*.pdf").split(separator: "\n")
         XCTAssertTrue(ls.contains("\(identifier!).out.pdf"))
     }
     
     func test_singlesided_shouldNotThrow_whenDeclinedToOverwrite() throws {
-        
+        XCTAssertEqual(fileService.counter, 0)
+        try fileService.save(PDFDocument(), named: "\(identifier!).out.pdf", to: ".")
+        XCTAssertEqual(fileService.counter, 1)
+        // Check if throws
+        XCTAssertEqual(printService.counter, 0)
+        XCTAssertEqual(fileService.counter, 1)
+        let args = try Arguments(input: "\(identifier!).pdf", output: ".", from: nil, to: nil)
+        console.response = "n"
+        XCTAssertNoThrow(try communicator.singlesided(args))
+        XCTAssertEqual(printService.counter, 0)
+        XCTAssertEqual(fileService.counter, 2)
     }
 
 }
---
+++ Tests/printdsTests/testable/TestableFileService.swift
@@ -0,0 +1,21 @@
+//
+//  File.swift
+//  
+//
+//  Created by Artem Zhukov on 22.01.22.
+//
+
+import Foundation
+import PDFKit.PDFDocument
+@testable import printds
+
+class TestableFileService: FileService {
+    
+    private(set) var counter = 0
+    
+    override func save(_ document: PDFDocument, named name: String, to path: String) throws {
+        counter += 1
+        try super.save(document, named: name, to: path)
+    }
+    
+}
